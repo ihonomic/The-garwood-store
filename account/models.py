99533b140771b87484b48f2e19c5a8f42f6f0e1d
wisdom199720 <agboighalewisdom@gmail.com>
2021-03-21 00:25:05 +0100

    new modification

--- account/models.py
+++ account/models.py
@@ -1,36 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    # this user attribute links the django user default model user - since we are going to be inheriting from it
+    # CASCADE- means django should delete this Customer and anything associated with it when the user is deleted
+    user = models.OneToOneField(User, null=True, blank=True,
+                                on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return self.name
+        return self.full_name
 
-class Products(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    price = models.FloatField(null=True)
-    discount_price = models.FloatField(null=True, blank=True,)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    discount_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     sold_out = models.BooleanField(default=False)
     description = models.TextField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    img = models.ImageField(blank=True, null=True, default="image.png")
+    img = models.ImageField(blank=True, null=True, default="profile_pic.png")
+
     def __str__(self):
         return self.name
 
+    # We will query our image field with this function, to catch errors when an image is deleted
+    @property
+    def imageURL(self):
+        try:
+            url = self.img.url
+        except:
+            url = ''
+        return url
+
+
 class Order(models.Model):
-    #creating drop down menu in status selection
+    # creating drop down menu in status selection
 
     STATUS = (
-        ('Pending','Pending'),
-        ('Out for delivery','Out for delivery'),
-        ('Delivered','Delivered'),
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
     )
-    customer = models.ForeignKey(Customer, null=True, on_delete= models.CASCADE)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS, blank=True)
     completed = models.BooleanField(default=False)
@@ -38,15 +56,17 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+
 class Cart(models.Model):
-    customer = models.ForeignKey(Customer, null=True,on_delete=models.CASCADE)
-    product = models.ForeignKey(Products, null=True, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     order = models.ForeignKey(Order, null=True, on_delete=models.CASCADE)
-    quantity = models.FloatField(null=True, default=1)
+    quantity = models.IntegerField(null=True, default=0)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.product.name
+
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
@@ -55,10 +75,10 @@ class ShippingAddress(models.Model):
     othername = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     phone2 = models.CharField(max_length=200, null=True)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     address = models.CharField(max_length=200, null=True)
     city = models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
--- account/views.py
+++ account/views.py
@@ -1,11 +1,90 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+
+# Create your views here.
+from .forms import CreateUserFrom
+from .models import *
+from .decorators import unauthenticated_user, allowed_users, admin_only
+
+
+
 
 # Create your views here.
 
+
+@unauthenticated_user
+def register(request):
+
+    form = CreateUserFrom()
+    if request.method == 'POST' :
+        form = CreateUserFrom(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+                name=user.username,
+                email=user.email,
+            )
+
+            messages.success(request, 'Account was created ' + username)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'account/register.html', context)
+
+
+
+@unauthenticated_user
+def loginpage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.info(request, 'Welcome to Garwood...')
+            return redirect('products')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
+    context = {}
+    return render(request, 'account/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('products')
+
+
+
+
+@login_required(login_url='login')
+@admin_only
 def home(request):
-    return render(request, 'account/home.html')
+    context = {}
+    return render(request, 'account/home.html', context)
+
+def AboutGarwood(request):
+    context = {}
+    return render(request, 'account/home.html', context)
 
 
-def products(request):
-    return render(request, 'account/products.html')
+def ProductStore(request):
+    # Querying all the products from the database
+    products = Product.objects.all()
+
+    context = {"products":products}
+    return render(request, 'account/products.html', context)
+
